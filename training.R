2c5d23a0c84f046e4548209fd9beebf042bdee80
umax46 <umax46>
2015-03-18 17:37:02 +0100

    Add training program for our neural network.\n Add a training set directory with spam messages

---
+++ training.R
@@ -0,0 +1,107 @@
+#For each token in the spam message corpus do
+#If layer is already exist in Innate_Input_Layer then 
+#  Innate_Input_Layer.msg_matched->Innate_Input_Layer.msg_matched + 1 
+#  Innate_Input_Layer.spam_matched->Innate_Input_Layer.spam_matched + spam_increment 
+#else
+#  Add token to Innate_Input_Layer 
+#  Innate_Input_Layer.msg_matched->Innate_Input_Layer.msg_matched + 1 
+#  Innate_Input_Layer.spam_matched->Innate_Input_Layer.spam_matched + spam_increment 
+#end if
+#end for
+library(stringr)
+
+# Set working directory
+setwd("/Users/umax46/TECI/Redes\ Neuronales\ y\ aprendizaje\ estadistico/Practicas/CLA_ANN");
+
+# Create an empty innate_input_layer
+innate_input_layers <- data.frame(token=character(), weight=numeric(), msg_matched=numeric(), spam_matched=numeric(), stringsAsFactors=FALSE) 
+num_node = 1000;
+
+# Function to get tokens from a message
+gimmie_tokens_from_message <- function(fileName) {
+  #file <- readChar(fileName, file.info(fileName)$size)
+  #mail_tokens <- strsplit(file, " ")[[1]]
+  #file <- scan(fileName,what="");
+  #file <- file [grep("[a-z]", file)];
+  #mail_tokens <- strsplit(file, " ", fixed = TRUE);
+  print("NOMBRE DE FICHERO:");
+  print(fileName);
+  
+  
+  file <- scan(fileName, what=character(),sep="", fileEncoding="UTF-8-BOM");
+  htlm_start_elements <- str_match(file,"<\\w+>");
+  htlm_end_elements <- str_match(file,"</\\w+>");
+  file <- file [! file %in% htlm_start_elements];
+  file <- file [! file %in% htlm_end_elements];
+  mail_tokens <- str_match(file,"\\w+");
+  mail_tokens <- na.omit(mail_tokens);
+  return(mail_tokens);
+}
+
+# Function to check if token exists in message
+exist_token <- function(mail_token) {
+  return(any(grepl(mail_token, innate_input_layers$token)));
+}
+
+# Function to retrieve a valid index for new token
+gimmie_index <- function(min_weight) {
+  current_index <- nrow(innate_input_layers);
+  if (current_index >= num_node) {
+    current_weight <- innate_input_layers$weight[which.min(innate_input_layers$weight)];
+    #print(current_weight);
+    if ( current_weight >= min_weight) {
+      return(0);
+    } else {
+      return(which.min(innate_input_layers$weight));
+    }  
+  } else {
+    return(current_index + 1);
+  }
+}
+
+# Set files from the training set
+training_set_directory <-"./training_set";
+#training_set_directory <-"./Prueba";
+spam<-list.files(path = training_set_directory);
+
+spam_increment = 0.5;
+for (i in 1:length(spam)) {
+  #print(spam[i]);
+  fileName <- paste(training_set_directory, spam[i], sep="/");
+  mail_tokens <- gimmie_tokens_from_message(fileName);
+  mail_tokens <- mail_tokens [! mail_tokens %in% ""] #to evict "" in mail_tokens
+  for (t in mail_tokens) {
+    t <- gsub("\\(", "", t); #to evict problems with '(text' without ')'
+    #print(t);
+    #print(class(t));
+    #print(any(grepl(t, innate_input_layers$token)));
+    exist <- exist_token(toupper(t));
+    #print(exist);
+   if (exist) {
+     #index <- which(apply(innate_input_layers, 2, function(x) any(grepl(t, innate_input_layers$token))))
+     #index <- index[1]
+     index <- which(innate_input_layers$token == t)
+      #  Innate_Input_Layer.msg_matched->Innate_Input_Layer.msg_matched + 1 
+      #  Innate_Input_Layer.spam_matched->Innate_Input_Layer.spam_matched + spam_increment 
+     if (length(index) == 1) {
+       #print(t);
+       #print(index);
+       #print(class(index))
+       #print(class(innate_input_layers$msg_matched));
+       #print(length(index));
+       new_weight <- as.numeric(innate_input_layers$spam_matched[index]) / (as.numeric(innate_input_layers$msg_matched[index]) + 1);
+       innate_input_layers$weight[index] <- new_weight;
+       innate_input_layers$msg_matched[index] <- as.numeric(innate_input_layers$msg_matched[index]) + 1;
+       innate_input_layers$spam_matched[index] <- as.numeric(innate_input_layers$spam_matched[index]) + spam_increment; 
+       
+     }
+    } else {
+      index <- gimmie_index(0.1);
+      #print(index);
+      if (index != 0) {
+        innate_input_layers[index, ] <- c(toupper(t), 0.1, 1, spam_increment);        
+      }
+    }
+  }
+}
+
